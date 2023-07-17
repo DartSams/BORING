e091a16c020a9a4e35ada80dd1fa154c1ba1fe96
dartsams <dartagnansams1@icloud.com>
2023-07-17 11:15:18 -0400

    cleaned up quizpage

--- RazorPagesUI/wwwroot/js/Quiz.js
+++ RazorPagesUI/wwwroot/js/Quiz.js
@@ -3,145 +3,169 @@ QUESTIONS = [
         questionNum: "question 1",
         question:"I have been bullied at school.",
         labels: ["Yes", "No", "Maybe"],
-        data: [24, 19, 16]
+        data: [24, 19, 16],
+        type:"single"
     },
     {
         questionNum: "question 2",
         question:"I was bullied in the... (Check all that apply)",
         labels: ["In the past month", "In the past month", "This term", "This year", "More than a year ago"],
-        data: [24, 19, 16, 7, 4]
+        data: [24, 19, 16, 7, 4],
+        type: "multiple-choice"
     },
     {
         questionNum: "question 3",
         question:"I was bullied...",
-        labels: ["1 time", "2-3 times", "4-5 times", "6 or more times", "I don't know"],
-        data: [24, 19, 16, 6, 3]
+        labels: ["1 time", "2-3 times", "4-5 times", "6 or more times", "I don\'t know"],
+        data: [24, 19, 16, 6, 3],
+        type: "single"
     },
     {
         questionNum: "question 4",
         question:"I was bullied by...",
         labels: ["A single student", "A group of students"],
-        data: [19, 16]
+        data: [19, 16],
+        type: "single"
     },
     {
         questionNum: "question 5",
         question:"Student who bullied me was a...",
         labels: ["A boy", "A girl", "Prefer not to say"],
-        data: [24, 19, 16]
+        data: [24, 19, 16],
+        type: "single"
     },
     {
         questionNum: "question 6",
         question:"The student who bullied me was...",
-        labels: ["Asian", "Black", "Hispanic", "Mutiracial ", "White", "I don’t know", "I prefer not to say "],
-        data: [24, 19, 16, 5, 37, 23, 15]
+        labels: ["Asian", "Black", "Hispanic", "Mutiracial ", "White", "I don\'t know", "I prefer not to say "],
+        data: [24, 19, 16, 5, 37, 23, 15],
+        type: "single"
     },
     {
         questionNum: "question 7",
         question:"The students who bullied me were...",
         labels: ["A group of boys ", "A group of girls", "Both boys and girls", "Prefer not to say"],
-        data: [24, 19, 16, 6]
+        data: [24, 19, 16, 6],
+        type: "single"
     },
     {
         questionNum: "question 8",
         question:"The students who bullied me were... (check all)",
-        labels: ["Asian", "Black", "Hispanic", "Mutiracial ", "White", "I don’t know", "I prefer not to say "],
-        data: [24, 19, 16, 5, 37, 23, 15]
+        labels: ["Asian", "Black", "Hispanic", "Mutiracial ", "White", "I don\'t know", "I prefer not to say "],
+        data: [24, 19, 16, 5, 37, 23, 15],
+        type: "multiple-choice"
     },
     {
         questionNum: "question 9",
         question:"The student who bullied me was...",
         labels: ["Younger than me", "Same age as me", "Older than me", "Not Sure"],
-        data: [24, 19, 16, 5]
+        data: [24, 19, 16, 5],
+        type: "single"
     },
     {
         questionNum: "question 10",
         question:"I was bullied... (Check all that apply)",
         labels: ["During school", "After school (on-campus)", "After school (off-campus)", "Other"],
-        data: [24, 19, 16, 23]
+        data: [24, 19, 16, 23],
+        type: "multiple-choice"
     },
     {
         questionNum: "question 11",
         question:"The students who bullied me were...",
         labels: ["Younger than me", "Same age as me", "Older than me", "Mixed aged group", "Not Sure"],
-        data: [24, 19, 16, 5, 37]
+        data: [24, 19, 16, 5, 37],
+        type: "single"
     },
     {
         questionNum: "question 12",
         question:"I was bullied... (Check all that apply)",
         labels: ["During school", "After school (on-campus)", "After school (off-campus)", "Other"],
-        data: [24, 19, 16, 23]
+        data: [24, 19, 16, 23],
+        type: "multiple-choice"
     },
     {
         questionNum: "question 13",
         question:"I was bullied... (Check all that apply)",
         labels: ["In class", "During free time", "In the restroom", "In the hallway", "At lunch", "During school activities ", "Locker room ", "Sports program at school", "Other "],
-        data: [24, 19, 16, 5, 37, 32, 23, 5, 17]
+        data: [24, 19, 16, 5, 37, 32, 23, 5, 17],
+        type: "multiple-choice"
     },
     {
         questionNum: "question 14",
         question:"What happened? (Check all that apply)",
         labels: ["Someone said something to me", "Someone did something to me", "Someone posted something about me"],
-        data: [24, 19, 16]
+        data: [24, 19, 16],
+        type: "multiple-choice"
     },
     {
         questionNum: "question 15",
         question:"What did the student or students say to you? ",
         labels: ["Threats", "Teasing", "Name-calling", "Ignored me", "Other "],
-        data: [24, 19, 16, 5, 37]
+        data: [24, 19, 16, 5, 37],
+        type: "single"
     },
     {
         questionNum: "question 16",
         question:"What did the student or students do to you?",
         labels: ["Hit me", "Pushed me", "Horseplay", "Made me do something I did not want to", "Other"],
-        data: [24, 19, 16, 5, 37]
+        data: [24, 19, 16, 5, 37],
+        type: "single"
     },
     {
         questionNum: "question 17",
         question:"How did the bullying occur?",
         labels: ["On a website/social media/app", "Through texting"],
-        data: [24, 19]
+        data: [24, 19],
+        type: "single"
     },
     {
         questionNum: "question 18",
         question:"They said these things to me because of...",
-        labels: ["who I date", "my sexual orientation", "who I hang out with", "my looks", "my faith", "my culture", "my race", "I do well in school", "I don’t do well in school", "what my family can afford", "what my family can’t afford", "Other"],
-        data: [24, 19, 16, 5, 24, 19, 16, 5, 23, 19, 24, 13]
+        labels: ["who I date", "my sexual orientation", "who I hang out with", "my looks", "my faith", "my culture", "my race", "I do well in school", "I don\'t do well in school", "what my family can afford", "what my family can\'t afford", "Other"],
+        data: [24, 19, 16, 5, 24, 19, 16, 5, 23, 19, 24, 13],
+        type: "single"
     },
     {
         questionNum: "question 19",
         question:"They did these things to me because of...",
-        labels: ["who I date", "my sexual orientation", "who I hang out with", "my looks", "my faith", "my culture", "my race", "I do well in school", "I don’t do well in school", "what my family can afford", "what my family can’t afford", "Other"],
-        data: [24, 19, 16, 5, 24, 19, 16, 5, 23, 19, 24, 13]
+        labels: ["who I date", "my sexual orientation", "who I hang out with", "my looks", "my faith", "my culture", "my race", "I do well in school", "I don\'t do well in school", "what my family can afford", "what my family can\'t afford", "Other"],
+        data: [24, 19, 16, 5, 24, 19, 16, 5, 23, 19, 24, 13],
+        type: "single"
     },
     {
         questionNum: "question 20",
         question: "What website or app did the student or students use?",
         labels: ["Facebook", "Whatsapp", "Instagram", "TikTok", "Snapchat", "Pinterest", "Reddit", "Twitter", "Yik Yak", "Youtube", "Other "],
-        data: [24, 19, 16, 5, 37, 24, 19, 16, 5, 23, 18]
+        data: [24, 19, 16, 5, 37, 24, 19, 16, 5, 23, 18],
+        type: "single"
     },
     {
         questionNum: "question 21",
         question: "What website or app did the student or students use?",
         labels: ["Facebook", "Whatsapp", "Instagram", "TikTok", "Snapchat", "Pinterest", "Reddit", "Twitter", "Yik Yak", "Youtube", "Other "],
-        data: [24, 19, 16, 5, 37, 24, 19, 16, 5, 37]
+        data: [24, 19, 16, 5, 37, 24, 19, 16, 5, 37],
+        type: "single"
     },
     {
         questionNum: "question 22",
         question: "What did the student post?",
         labels: ["Pictures of me", "Mean messages about me", "Spreading rumors about me", "Other"],
-        data: [24, 19, 16, 5]
+        data: [24, 19, 16, 5],
+        type: "single"
     },
     {
         questionNum: "question 23",
         question: "They posted these things about because of...",
-        labels: ["who I date", "my sexual orientation", "who I hang out with", "my looks", "my faith", "my culture", "my race", "I do well in school", "I don’t do well in school", "what my family can afford", "what my family can’t afford", "Other"],
-        data: [24, 19, 16, 5, 24, 19, 32, 67, 78, 19, 24, 13]
+        labels: ["who I date", "my sexual orientation", "who I hang out with", "my looks", "my faith", "my culture", "my race", "I do well in school", "I don\'t do well in school", "what my family can afford", "what my family can\'t afford", "Other"],
+        data: [24, 19, 16, 5, 24, 19, 32, 67, 78, 19, 24, 13],
+        type: "single"
     },
     {
         questionNum: "question 24",
         question: "How did you respond?",
         labels: ["Ignored them", "Told the person to stop", "I fought back", "Told a friend or peer", "Told my parents", "Told a teacher", "Reported it to the school", "I reported it to the school resource officer/police", "Other "],
-        data: [24, 19, 16, 5, 37, 24, 19, 16, 5]
+        data: [24, 19, 16, 5, 37, 24, 19, 16, 5],
+        type: "single"
     },
 
 ]
@@ -151,11 +175,26 @@ updateQuestion()
 
 function updateQuestion() {
     document.querySelector(".question").innerText = QUESTIONS[currentQuestion].question
-    generateChoices(QUESTIONS[currentQuestion].labels)
+    generateChoices(QUESTIONS[currentQuestion].labels, QUESTIONS[currentQuestion].type)
 }
 
-function generateChoices(choices) {
+function generateChoices(choices,questionType) {
     let choiceBody = document.querySelector(".question-choices")
+    if (choices.length <= 3) {
+        choiceBody.style.cssText = `
+            display:flex;
+            flex-direction:column;
+            align-items:baseline;
+            flex-basis:50px;
+            width: 100%;
+        `
+    } else if (choices.length >= 7) {
+        choiceBody.style.cssText = `
+            display:flex;
+            flex-direction:row;
+            align-items:baseline;
+        `
+    }
     choiceBody.innerHTML = ""
 
     for (let i = 0; i < choices.length;i++) {
@@ -164,7 +203,11 @@ function generateChoices(choices) {
 
         let input = document.createElement("input");
         input.name = "choice"
-        input.type = "radio"
+        if (questionType == "single") {
+            input.type = "radio"
+        } else if (questionType == "multiple-choice") {
+            input.type = "checkbox"
+        }
         input.value = choices[i]
 
         let label = document.createElement("label");
@@ -174,15 +217,24 @@ function generateChoices(choices) {
         choice.append(label)
         choiceBody.append(choice)
     }
-}
+} //arguments are 1. a list of choices for a question 2. the type of answer expected for the question radio if single answer but checkbox to multiple-choice
 
 document.querySelector(".next-button").addEventListener("click", function () {
+    let getSelectedValue;
     if (currentQuestion <= QUESTIONS.length) { 
         //get the selected answer
         // if radio button answer is Yes then skip logic
+        if (QUESTIONS[currentQuestion].type == "single") {
+            getSelectedValue = document.querySelector('input[type="radio"]:checked'); //for multiple choice questions gets all selected inputs 
+            QUESTIONS[currentQuestion].answer = getSelectedValue.value //sets the answer to the dictionary of questions
+        } else if (QUESTIONS[currentQuestion].type == "multiple-choice") {
+            getSelectedValue = document.querySelectorAll('input[type="checkbox"]:checked'); //for multiple choice questions gets all selected inputs
+            //QUESTIONS[currentQuestion].answer = getSelectedValue.value //sets the answer to the dictionary of questions
 
-        let getSelectedValue = document.querySelector('input[name="choice"]:checked'); //gets all the input elements and finds the one that is checked
-        QUESTIONS[currentQuestion].answer = getSelectedValue.value //sets the answer to the dictionary of questions
+            multipleChoices = Array.from(getSelectedValue).map(x => x.value)
+            QUESTIONS[currentQuestion].answer = multipleChoices
+        } 
+        
 
         if (QUESTIONS[0].answer == "No") {
             for (let i = 0; i < QUESTIONS.length; i++) {
